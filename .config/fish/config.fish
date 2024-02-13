92bfae75c4cd950c3e51799932575bdd21eedd8d
Alexander Wolff <wolffaxn@gmail.com>
2024-02-13 20:41:01 +0100

    feat(fish): initial configuration

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+# homebrew
+if test -d "/opt/homebrew"
+    set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+    set -gx HOMEBREW_PREFIX "/opt/homebrew";
+    set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+end
+
+set -gxp PATH $HOMEBREW_PREFIX/sbin $HOMEBREW_PREFIX/bin /usr/local/sbin /usr/local/bin
+set -gx EDITOR nvim
+set -gx GPG_TTY (tty)
+
+# don't show any greetings
+set fish_greeting ""
+
+# brew install jump (https://github.com/gsamokovarov/jump)
+status --is-interactive; and source (jump shell fish | psub)
+
+eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
