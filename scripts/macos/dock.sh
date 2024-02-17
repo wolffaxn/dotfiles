6f530f2f77f8e2f87b466087e423c3362c301fbb
Alexander Wolff <wolffaxn@gmail.com>
2024-02-17 22:24:40 +0100

    feat(macos): rearrange dock

---
+++ scripts/macos/dock.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# Rearrange dock icons.
+
+set -e
+
+dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Finder.app"
+dockutil --no-restart --add "/Applications/Safari.app"
+dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Brave Browser.app"
+dockutil --no-restart --add "/Applications/iTerm.app"
+dockutil --no-restart --add "/Applications/Spotify.app"
+
+killall Dock
