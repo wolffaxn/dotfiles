8dfbe3839d263e4c9e1d13450ae9860cfb4e8e19
Alexander Wolff <wolffaxn@gmail.com>
2024-02-18 11:16:16 +0100

    feat(macos): add vscodium

--- scripts/macos/dock.sh
+++ scripts/macos/dock.sh
@@ -10,6 +10,7 @@ dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/Brave Browser.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
+dockutil --no-restart --add "/Applications/VSCodium.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 
 killall Dock
