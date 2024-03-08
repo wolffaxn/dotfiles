89cc15b1ef8623fd2a8e47d4a6b4e422e6c3a465
Alexander Wolff <wolffaxn@gmail.com>
2024-03-08 21:02:16 +0100

    feat(fish): more abbrevations

--- config/fish/conf.d/docker.fish
+++ config/fish/conf.d/docker.fish
@@ -1,3 +1,8 @@
 # docker abbrevations
-abbr -a d "docker"
 abbr -a dc "docker-compose"
+abbr -a dk "docker"
+abbr -a dkps "docker ps -a"
+abbr -a dkrm "docker rm (docker ps -a -q)"
+abbr -a dkrmi "docker rmi (docker images -q)"
+abbr -a dm "docker-machine"
+abbr -a dmev "eval (docker-machine env default)"
--- config/fish/conf.d/git.fish
+++ config/fish/conf.d/git.fish
@@ -1,34 +1,24 @@
 # git abbrevations
 
-# add file
+abbr -a g "git"
 abbr -a ga "git add"
-# add all
 abbr -a gaa "git add --all"
-# branch
-abbr -a gb "git branch"
-# commit file
-abbr -a gca "git commit -m"
-# checkout
+abbr -a gb "git branch -v"
+abbr -a gc "git commit -s"
+abbr -a gca "git commit -sa"
+abbr -a gcm "git commit -sm"
+abbr -a gcam "git commit -sam"
 abbr -a gco "git checkout"
-# checkout branch
 abbr -a gcob "git checkout -b"
-# discard changes in the working directory
-abbr -a gcod "git checkout -- ."
-# cherry-pick
 abbr -a gcp "git cherry-pick"
-# diff
 abbr -a gd "git diff"
-# merge
 abbr -a gm "git merge --no-ff"
-# push
-abbr -a gp "git push"
-# push including tags
+abbr -a gl "git pull --prune"
+abbr -a gp "git push origin HEAD"
+abbr -a gpa "git push origin --all"
 abbr -a gpt "git push --tags"
-# removed staged files
-abbr -a grm "git reset HEAD"
-# reset to <commit>
 abbr -a grh "git reset --hard"
-# show status
-abbr -a gs "git status"
-# show status without untracked file
+abbr -a gs "git status -sb"
 abbr -a gsu "git status -uno"
+abbr -a gw "git switch"
+abbr -a gwc "git switch -c"
