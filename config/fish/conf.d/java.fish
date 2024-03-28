bb6c1a94034b0d069af2224080778e7d036fd03a
Alexander Wolff <wolffaxn@gmail.com>
2024-03-28 21:24:55 +0100

    restructure dotfiles

--- config/fish/conf.d/java.fish
+++ config/fish/conf.d/java.fish
@@ -1,1 +1,12 @@
 status --is-interactive; and jenv init - | source
+
+# Gradle home
+set -x GRADLE_HOME /usr/local/opt/gradle/libexec
+set -x GRADLE_OPTS "-Dorg.gradle.daemon=true"
+
+# Java home
+set -x JAVA_HOME (/usr/libexec/java_home -v 14)
+
+# Maven home
+set -x M2_HOME /usr/local/opt/maven/libexec
+set -x MAVEN_OPTS "-Xmx1024M"
