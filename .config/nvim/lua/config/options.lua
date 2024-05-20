9e14598ced7d2060f48192f836604f6493c3ab77
Alexander Wolff <wolffaxn@gmail.com>
2024-05-20 19:05:56 +0200

    feat(nvim): additional options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+-- file encoding
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+
+-- indentation
+opt.autoindent = true
+opt.backspace = { "start", "eol", "indent" }
+opt.smarttab = true
+opt.softtabstop = 2
