383e276c62c218e86d85b208801ce7a2d95aac49
Alexander Wolff <wolffaxn@gmail.com>
2024-05-20 15:37:13 +0200

    feat(nvim): show hidden files by default

---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
+        show_hidden_count = true,
+        visible = true,
+      },
+    },
+  },
+}
