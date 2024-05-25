ab28bf737bfdfd0b5151d1b21384e4174ca0be4e
Alexander Wolff <wolffaxn@gmail.com>
2024-05-25 16:34:14 +0200

    fix(fish): disable homebrew analytics

--- .config/fish/conf.d/brew.fish
+++ .config/fish/conf.d/brew.fish
@@ -6,6 +6,8 @@ if test -d /opt/homebrew
     /opt/homebrew/bin/brew shellenv | source
 end
 
+# disable analytics
+set -x HOMEBREW_NO_ANALYTICS 1
 # stop brew install from update everything
 set -x HOMEBREW_NO_AUTO_UPDATE 1
 # disable annoying hits
