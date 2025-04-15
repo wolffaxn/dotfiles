08161dcf8d715214f76fc6871c9a67a16096f328
Alexander Wolff <wolffaxn@gmail.com>
2025-04-15 17:32:46 +0200

    feat(install): replace dotbot with stow

--- install/macos/brew.sh
+++ install/macos/brew.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # use latest homebrew and upgrade already installed formulae
 brew update && brew upgrade
 # install new casks, formulae etc.
-brew bundle install --file=./Brewfile
+brew bundle install --file="${BASEDIR}/Brewfile"
 # remove outdated versions from cellar
 brew cleanup
