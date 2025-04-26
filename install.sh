270ecace7988b350b29c74256e76cc349e42bc4a
Alexander Wolff <wolffaxn@gmail.com>
2025-04-26 21:34:18 +0200

    feat(install): add install script

--- install.sh
+++ install.sh
@@ -2,25 +2,32 @@
 
 set -e
 
-echo "Installing dotfiles"
+echo "Installing dotfiles..."
 
-echo "Installing submodule(s)"
+echo "Installing submodule(s)..."
 git submodule update --init --recursive
 
 if [ $(uname) == "Darwin" ]; then
   echo "Running on macOS"
 
-  echo "Brewing packages"
+  echo "Installing Xcode cli tools..."
+  xcode-select --install
+
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew analytics off
+  
+  echo "Installing Brew Formulae..."
   source install/macos/brew.sh
 
-  echo "Updating defaults"
+  echo "Updating defaults..."
   source install/macos/defaults.sh
 
-  echo "Rearrange dock"
+  echo "Rearrange dock..."
   source install/macos/dock.sh
 fi
 
-echo "Symlink dotfiles"
+echo "Symlink dotfiles..."
 stow . -t ~
 
 echo "Configuring zsh as default shell"
