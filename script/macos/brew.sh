afdbb2b09df84ffff24002c3a76eceba573f388e
Alexander Wolff <wolffaxn@gmail.com>
2025-05-08 21:14:19 +0200

    feat(macos): more casks

--- script/macos/brew.sh
+++ script/macos/brew.sh
@@ -5,8 +5,7 @@ set -e
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # use latest homebrew and upgrade already installed formulae
-brew update && brew upgrade
-# install new casks, formulae etc.
-brew bundle install --file="${BASEDIR}/Brewfile"
-# remove outdated versions from cellar
+brew update
+brew bundle install --cleanup --file="${BASEDIR}/Brewfile"
+brew upgrade
 brew cleanup
