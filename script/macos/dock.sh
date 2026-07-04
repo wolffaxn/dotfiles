7c3fdc053c327f7a57313c48ba626d3c08f17de1
Alexander Wolff <wolffaxn@gmail.com>
2026-07-04 23:16:05 +0200

    chore(macos): reorganize dock

--- script/macos/dock.sh
+++ script/macos/dock.sh
@@ -7,10 +7,8 @@ set -e
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/Applications/Brave Browser.app"
-dockutil --no-restart --add "/Applications/Alacritty.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Ghostty.app"
-dockutil --no-restart --add "/Applications/VSCodium.app"
 dockutil --no-restart --add "/Applications/Obsidian.app"
 
 killall Dock
