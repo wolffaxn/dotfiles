f43aa27f55ec749736329a323a4ed6693de84e5a
Alexander Wolff <wolffaxn@gmail.com>
2026-07-10 22:45:42 +0200

    feat(macos): rearrange dock

--- scripts/macos/dock.sh
+++ scripts/macos/dock.sh
@@ -7,8 +7,11 @@ set -e
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Docker.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Ghostty.app"
 dockutil --no-restart --add "/Applications/Obsidian.app"
+dockutil --no-restart --add "/Applications/Spotify.app"
+dockutil --no-restart --add "/Applications/KeePassXC.app"
 
 killall Dock
